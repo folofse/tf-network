b9e9a469a188c7d9d755a9477c0138a6bb0119eb
Olof Larsson <olof@folof.se>
2024-01-05 16:39:12 +0100

    feat: add deniseaddario.se

---
+++ hosted-zones/deniseaddario-se.tf
@@ -0,0 +1,23 @@
+module "brf_leopold" {
+  source = "../modules/hosted-zone"
+
+  domain_name   = "deniseaddario.se"
+  transfer_lock = false // Not supported by .se
+
+  admin_contact      = var.default_contact
+  registrant_contact = var.default_contact
+  tech_contact       = var.default_contact
+
+  admin_contact_extra_params = {
+    SE_ID_NUMBER  = "[SE]830512-4615",
+    BIRTH_COUNTRY = "SE"
+  }
+  registrant_contact_extra_params = {
+    SE_ID_NUMBER  = "[SE]830512-4615"
+    BIRTH_COUNTRY = "SE"
+  }
+  tech_contact_extra_params = {
+    SE_ID_NUMBER  = "[SE]830512-4615"
+    BIRTH_COUNTRY = "SE"
+  }
+}
