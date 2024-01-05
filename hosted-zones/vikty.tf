94ee4f29aa9e6cdde8a227a939658b3b08735d97
Olof Larsson <olof@folof.se>
2024-01-05 16:14:50 +0100

    fix: whois information, add BIRTH_COUNTRY

--- hosted-zones/vikty.tf
+++ hosted-zones/vikty.tf
@@ -8,12 +8,15 @@ module "brf_leopold" {
   tech_contact       = var.default_contact
 
   admin_contact_extra_params = {
-    "SE_ID_NUMBER" = "[SE]830512-4615"
+    SE_ID_NUMBER  = "[SE]830512-4615",
+    BIRTH_COUNTRY = "SE"
   }
   registrant_contact_extra_params = {
-    "SE_ID_NUMBER" = "[SE]830512-4615"
+    SE_ID_NUMBER  = "[SE]830512-4615"
+    BIRTH_COUNTRY = "SE"
   }
   tech_contact_extra_params = {
-    "SE_ID_NUMBER" = "[SE]830512-4615"
+    SE_ID_NUMBER  = "[SE]830512-4615"
+    BIRTH_COUNTRY = "SE"
   }
 }
