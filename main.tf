1dac87d811892078c67acc6d2fbec96c8be85ded
Olof Larsson <olof@folof.se>
2024-01-18 22:37:15 +0100

    fix: remove import blocks

--- main.tf
+++ main.tf
@@ -7,13 +7,3 @@ terraform {
     profile        = "backend"
   }
 }
-
-import {
-  to = module.hosted_zones.aws_route53_record.brfleopold_se_ns
-  id = "Z016306414B1XLEMVM0AT_brfleopold.se_NS"
-}
-
-import {
-  to = module.hosted_zones.aws_route53_record.brfleopold_se_soa
-  id = "Z016306414B1XLEMVM0AT_brfleopold.se_SOA"
-}
