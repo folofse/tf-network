088808196429208245d09fb866b5eff513f0ecb5
Olof Larsson <olof@folof.se>
2024-01-18 21:43:07 +0100

    fix: add ns and soa records to brfleopold

--- hosted-zones/brfleopold-se.tf
+++ hosted-zones/brfleopold-se.tf
@@ -12,3 +12,28 @@ module "brfleopold_se" {
   registrant_contact_extra_params = local.extra_params
   tech_contact_extra_params       = local.extra_params
 }
+
+resource "aws_route53_record" "brfleopold_se_ns" {
+  zone_id = module.brfleopold_se.route53_zone_id
+  name    = ""
+  type    = "NS"
+  ttl     = 172800
+
+  records = [
+    "ns-1937.awsdns-50.co.uk.",
+    "ns-684.awsdns-21.net.",
+    "ns-57.awsdns-07.com.",
+    "ns-1241.awsdns-27.org."
+  ]
+}
+
+resource "aws_route53_record" "brfleopold_se_soa" {
+  zone_id = module.brfleopold_se.route53_zone_id
+  name    = ""
+  type    = "SOA"
+  ttl     = 900
+
+  records = [
+    "ns-1937.awsdns-50.co.uk. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400",
+  ]
+}
