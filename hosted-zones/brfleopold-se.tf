76d3da96cab4dad4f06dccb51a3c9a995e5e59bc
Olof Larsson <olof@folof.se>
2024-01-18 15:04:06 +0100

    fix: remove unused ns records

--- hosted-zones/brfleopold-se.tf
+++ hosted-zones/brfleopold-se.tf
@@ -12,17 +12,3 @@ module "brfleopold_se" {
   registrant_contact_extra_params = local.extra_params
   tech_contact_extra_params       = local.extra_params
 }
-
-resource "aws_route53_record" "brfleopold_se_ns" {
-  zone_id = module.brfleopold_se.route53_zone_id
-  name    = ""
-  type    = "NS"
-  ttl     = 3600
-
-  records = [
-    "ns-1937.awsdns-50.co.uk.",
-    "ns-684.awsdns-21.net.",
-    "ns-57.awsdns-07.com.",
-    "ns-1241.awsdns-27.org."
-  ]
-}
