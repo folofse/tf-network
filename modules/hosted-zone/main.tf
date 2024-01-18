a72142ec8b28b726f2d03de19f44a6ce6ac7851e
Olof Larsson <olof@folof.se>
2024-01-18 23:19:24 +0100

    fix: format

--- modules/hosted-zone/main.tf
+++ modules/hosted-zone/main.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_zone" "zone" {
-  name = var.domain_name
+  name    = var.domain_name
   comment = length(var.domain_name_servers) > 0 ? "Not used - domain points to external NS records" : "Managed by Terraform"
 }
 
